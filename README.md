f73cf2561a0e4d43e80f0f8e1721686373969687
mpurnyn <mpurnyn@gmail.com>
2021-11-16 09:30:39 +0900

    import names dataset; shuffle by seed

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# data_masking
+# Data Masking
 Tools for data obfuscation/masking allowing useable data while allowing for calculating original 1-1 relationship with data.
+
+
+# Name Shuffler
+A tool for being able take in names and output masked names/emails to retain useability in a dataset while removing real-world data for handing over to another party. The masked data retains a 1-1 relationship to the data which can then be reaccessed via a key.
+
+This tool imports a large dataset of names for the "shuffling" of names.
+
---
+++ name_shuffler.py
@@ -0,0 +1,43 @@
+from names_dataset import NameDatasetV1 # v1
+import random
+
+random.seed("602888")
+
+m = NameDatasetV1()
+first_names = list(m.first_names)
+first_names.sort()
+random.shuffle(first_names)
+
+#returns a list of first name and last name
+def split_full_name(fullname):
+    split_name = fullname.split(" ")
+
+# make a fake email from the fullname and a domain address
+def make_email(fullname, domain_name):
+    email = fullname + "@" + domain_name
+    return email
+
+# make a fake name from the first and last names
+def next_full_name(first_name, last_name):
+    full = next_first_name(first_name)
+    full = full + " " + next_last_name(last_name)
+    return full
+
+# input a name and get the next name in the dataset
+def next_first_name(first_name):
+    first = ""
+    return first
+
+# input a name and get the next name in the dataset
+def next_last_name(last_name):
+    last = ""
+    return last
+
+if __name__ == "__main__":
+    fn = first_names.copy()
+    i = fn.index("peter")
+    next = fn.pop(i+1)
+    print(next)
+    next = first_names.pop(i+1)
+    print(next)
+    print(make_email(next, "fake.kit.jp"))
